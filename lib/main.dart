94d91eb05a042e993989e0245bdaed7bc751bb9b
Shailesh Mota <shailesh.mota@vestiairecollective.com>
2020-06-03 19:36:25 +0200

    use autoformatter to make code more readable

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => new _MyAppState();
-
 }
 
 class _MyAppState extends State<MyApp> {
@@ -33,73 +32,69 @@ class _MyAppState extends State<MyApp> {
         appBar: new AppBar(
           title: const Text('Test App'),
         ),
-        body:Center( child: Column(children: <Widget>[
-          Row(
-         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              CountryCodePicker(
-                onChanged: print,
-                // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
-                initialSelection: 'IT',
-                favorite: ['+39', 'FR'],
-               // countryFilter: ['IT', 'FR'],
-                showFlagDialog: false,
-                comparator: (a, b) => b.name.compareTo(a.name),
-                //Get the country information relevant to the initial selection
-                onInit: (code) =>
-                    print("on init ${code.name} ${code.dialCode}"),
-              ),
-                Expanded(
-                 child: TextField(
-                         decoration: new InputDecoration(labelText: "Enter your number"),
-                        keyboardType: TextInputType.number,
-                        inputFormatters: <TextInputFormatter>[
-                          WhitelistingTextInputFormatter.digitsOnly
-                        ], // Only numbers can be entered
-                      ),
-               ),
-                    ],
-
-          ),
-          Row(
-            children: <Widget>[
-              const SizedBox(height: 30),
-              RaisedButton(
-                onPressed: () {
-                  //context=this.context;
-                  navigateToSubPage(context);
-
-                },
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(0.0),
-                child: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: <Color>[
-                        Color(0xFF0D47A1),
-                        Color(0xFF1976D2),
-                        Color(0xFF42A5F5),
-                      ],
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(10.0),
-                  child:
-                  const Text('Generate OTP', style: TextStyle(fontSize: 20)),
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CountryCodePicker(
+                  onChanged: print,
+                  // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                  initialSelection: 'IT',
+                  favorite: ['+39', 'FR'],
+                  // countryFilter: ['IT', 'FR'],
+                  showFlagDialog: false,
+                  comparator: (a, b) => b.name.compareTo(a.name),
+                  //Get the country information relevant to the initial selection
+                  onInit: (code) =>
+                      print("on init ${code.name} ${code.dialCode}"),
                 ),
-              ),
-            ],
-
-
-
-
-          )
-
-],
-      )
-        ),
-        ),
+                Expanded(
+                  child: TextField(
+                    decoration:
+                        new InputDecoration(labelText: "Enter your number"),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      WhitelistingTextInputFormatter.digitsOnly
+                    ], // Only numbers can be entered
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                const SizedBox(height: 30),
+                RaisedButton(
+                  onPressed: () {
+                    //context=this.context;
+                    navigateToSubPage(context);
+                  },
+                  textColor: Colors.white,
+                  padding: const EdgeInsets.all(0.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF0D47A1),
+                          Color(0xFF1976D2),
+                          Color(0xFF42A5F5),
+                        ],
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(10.0),
+                    child: const Text('Generate OTP',
+                        style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+              ],
+            )
+          ],
+        )),
+      ),
     );
   }
+
   Future navigateToSubPage(context) async {
     Navigator.push(context, MaterialPageRoute(builder: (context) => SubPage()));
   }
